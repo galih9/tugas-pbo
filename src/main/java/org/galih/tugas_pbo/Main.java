7ff440c8f6185e23de18df37334bcbc75f8906f2
galih9 <galihbolang07@gmail.com>
2025-05-24 19:32:53 +0700

    Add Jadwal class to manage course schedules with date and time handling

--- src/main/java/org/galih/tugas_pbo/Main.java
+++ src/main/java/org/galih/tugas_pbo/Main.java
@@ -1,5 +1,7 @@
 package org.galih.tugas_pbo;
 
+import java.time.LocalDateTime;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -13,3 +15,31 @@ class User {
     User(String nim, String name, String programStudi) {
     }
 }
+
+class Jadwal {
+    static int counter = 0;
+    int id;
+    String namaKursus;
+    String tanggal;
+    String jam;
+    String dosen;
+    boolean selesai;
+
+    Jadwal(String namaKursus, String tanggal, String jam, String dosen) {
+        this.id = ++counter;
+        this.namaKursus = namaKursus;
+        this.tanggal = tanggal;
+        this.jam = jam;
+        this.dosen = dosen;
+        this.selesai = false;
+    }
+
+    LocalDateTime getDateTime() {
+        try {
+            return LocalDateTime.parse(this.tanggal + "T" + this.jam);
+        } catch (Exception e) {
+            return LocalDateTime.MIN;
+        }
+    }
+}
+
